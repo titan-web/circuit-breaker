d0e54cf8fab47994c0e90b395a81cfe31854c246
tjf94432 <tjf94432@alibaba-inc.com>
2016-05-19 00:44:30 +0800

    modify BreakerState logic

--- fuses/__init__.py
+++ fuses/__init__.py
@@ -6,6 +6,7 @@ from time import time
 from threading import RLock
 import functools
 import contextlib
+import random
 
 __all__ = ("breaker", "Fuses")
 
@@ -30,7 +31,7 @@ def circuit(fuses):
     :param fuses: an instance of Fuses
     """
     try:
-        yield fuses.handle()
+        yield fuses.pre_handle()
     except Exception as exp:
         except_name = exp.__class__.__name__
         if except_name in fuses.exception_list():
@@ -56,6 +57,37 @@ class Fuses(object):
 
         self._fail_counter = 0
 
+    @property
+    def last_time(self):
+        return self._last_time
+
+    @last_time.setter
+    def last_time(self, time_):
+        self._last_time = time_
+
+    @property
+    def reset_timeout(self):
+        return self._timeout
+
+    @reset_timeout.setter
+    def reset_timeout(self, timeout):
+        self._timeout = timeout
+
+    @property
+    def backoff_cap(self):
+        return self._backoff_cap
+
+    @property
+    def with_jitter(self):
+        return self._with_jitter
+
+    @property
+    def fail_counter(self):
+        return self._fail_counter
+
+    def reset_fail_counter(self):
+        self._fail_counter = 0
+
     def exception_list(self):
         return self._exception_list
 
@@ -77,6 +109,15 @@ class Fuses(object):
     def incr_counter(self):
         self._fail_counter += 1
 
+    def is_melting_point(self):
+        if self._fail_counter > self._max_fails:
+            return True
+        return False
+
+    def pre_handle(self):
+        with self._lock:
+            self._cur_state.pre_handle()
+
     def handle(self):
         with self._lock:
             self._cur_state.handle()
@@ -97,20 +138,35 @@ class BreakerState(object):
     def __init__(self, fuses):
         self._fuses = fuses
 
+    def pre_handle(self):
+        raise NotImplementedError("Must implement pre_handle!")
+
     def handle(self):
         pass
 
     def success(self):
-        NotImplementedError("Must implement success!")
+        raise NotImplementedError("Must implement success!")
 
     def error(self):
-        NotImplementedError("Must implement error!")
+        raise NotImplementedError("Must implement error!")
 
 
 class BreakerOpenState(BreakerState):
 
     def __init__(self, fuses):
         super(BreakerOpenState, self).__init__(fuses)
+
+    def pre_handle(self):
+        now = time()
+        reset_timeout = self._fuses.reset_timeout
+        if self._fuses.backoff_cap:
+            reset_timeout = self._fuses.reset_timeout * (2 ** self._fuses.fail_counter)
+            reset_timeout = min(reset_timeout, self._fuses.backoff_cap)
+        if self._fuses.with_jitter:
+            reset_timeout = random.random() * reset_timeout
+        if now > (self._fuses.last_time + reset_timeout):
+            self._fuses.half_open()
+        return self._fuses
 
     def success(self):
         pass
@@ -124,12 +180,18 @@ class BreakerClosedState(BreakerState):
     def __init__(self, fuses):
         super(BreakerClosedState, self).__init__(fuses)
 
+    def pre_handle(self):
+        return self._fuses
+
     def success(self):
         pass
 
     def error(self):
-        """ `close` state handle where error"""
-        self._fuses.incr_counter()
+        """ `close` state handle error"""
+        if self._fuses.is_melting_point():
+            self._fuses.open()
+        else:
+            self._fuses.incr_counter()
 
 
 class BreakerHalfOpenState(BreakerState):
@@ -137,11 +199,15 @@ class BreakerHalfOpenState(BreakerState):
     def __init__(self, fuses):
         super(BreakerHalfOpenState, self).__init__(fuses)
 
+    def pre_handle(self):
+        return self._fuses
+
     def success(self):
-        pass
+        self._fuses.reset_fail_counter()
+        self._fuses.close()
 
     def error(self):
-        pass
+        self._fuses.open()
 
 if __name__ == "__main__":
     f = Fuses(5, 10, [ValueError])
